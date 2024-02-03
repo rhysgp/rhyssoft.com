d3a54d9b62408c9cfd30205ccdafff32cf42efc6
Rhys Parsons <rhys@rhyssoft.com>
2024-02-03 16:51:53 +0000

    Put blog inside content section.

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -2,24 +2,25 @@
 import { useRoute } from 'vue-router'
 import {type BlogParagraph, blogs} from '@/blogs'
 import BlogPara from "@/components/BlogPara.vue";
+import Content from "@/components/Content.vue";
 
 
 const props = defineProps<{
   id: number
 }>();
 
-
 const route = useRoute()
 
 const blog: BlogParagraph[] = blogs()[Number(route.params.id)]
 
-
 </script>
 
 <template>
-  <template v-for="para in blog">
-    <BlogPara :style="para.style" :texts="para.texts" />
-  </template>
+  <Content>
+    <template v-for="para in blog">
+      <BlogPara :style="para.style" :texts="para.texts" />
+    </template>
+  </Content>
 </template>
 
 <style scoped>
