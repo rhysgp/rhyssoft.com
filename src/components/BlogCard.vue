bca749df28b4123d8e3b2efeafe9215a9f27fa4a
Rhys Parsons <rhys@rhyssoft.com>
2024-02-04 12:05:21 +0000

    Use 'moment' to format date.

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import moment from 'moment';
 
 import type {BlogParagraph} from "@/blogs";
 
@@ -6,15 +7,19 @@ const props = defineProps<{
   blog: BlogParagraph[]
   index: number;
 }>();
-</script>
 
+const formatDate = (s: string) => {
+  return moment(s).format('LL');
+}
+
+</script>
 <template>
   <router-link :to="'/blog/' + index">
     <div class="card">
       <div class="title">{{ blog[0].texts[0].text }}</div>
       <hr>
       <div class="intro">{{ blog[2].texts[0].text }}</div>
-      <div class="date">{{ blog[1].texts[0].text }}</div>
+      <div class="date">{{ formatDate(blog[1].texts[0].text) }}</div>
     </div>
   </router-link>
 </template>
