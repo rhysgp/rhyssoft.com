48ae38eb99460044555bc92ec2af008114fd8c14
Rhys Parsons <rhys@rhyssoft.com>
2024-02-04 17:36:51 +0000

    Add cutive-mono config for tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
       "./src/**/*.{js,ts,jsx,tsx,vue}"
   ],
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+            'cutive-mono': ['"Cutive Mono"']
+        }
+    },
   },
   plugins: [],
 }
