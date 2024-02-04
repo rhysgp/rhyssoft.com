b41d21286eaaad24069e6df74e023adae57ef6e4
Rhys Parsons <rhys@rhyssoft.com>
2024-02-04 17:14:15 +0000

    Change styling of menu items.

--- src/components/NavLink.vue
+++ src/components/NavLink.vue
@@ -17,9 +17,9 @@ const path = computed(() => { return "/" + props.page });
 
 <template>
   <template v-if="route.path == path">
-    <RouterLink class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" :to="path" aria-current="page">{{label}}</RouterLink>
+    <RouterLink class="text-white px-3 py-2 text-sm font-medium border-b border-color-[#cbdec9]" :to="path" aria-current="page">{{label}}</RouterLink>
   </template>
   <template v-else>
-    <RouterLink class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" :to="path">{{label}}</RouterLink>
+    <RouterLink class="text-white px-3 py-2 text-sm font-medium border-b border-transparent hover:border-[#cbdec9]" :to="path">{{label}}</RouterLink>
   </template>
 </template>
