5c5a9342f9729e91bae8e6bef4dd98e169a2d3f4
Rhys Parsons <rhys@rhyssoft.com>
2024-03-06 11:59:59 +0000

    Clean up vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
 import { resolve } from 'path'
-import { glob } from 'glob';
-import path from 'node:path';
 
 import blog from './rollup-plugin/rollup-plugin-blog-processor.ts'
-import sitemap from './rollup-plugin/roller-plugin-sitemap-generator.ts'
+//import sitemap from './rollup-plugin/roller-plugin-sitemap-generator.ts'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-const baseFilename = (file: string) => {
-  return path.relative(
-    'blogs',
-    file.slice(0, file.length - path.extname(file).length)
-  );
-}
-
-const blogs =
-  Object.fromEntries(
-    glob.sync(`${resolve(__dirname, 'blogs')}/**/*.md`).map(
-      file => [ baseFilename(file), file ]
-    )
-  )
-
-console.log(JSON.stringify(blogs, null, 2))
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -45,7 +27,6 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        // ...blogs,
         main: resolve(__dirname, 'index.html'),
       }
     }
